6a110e44b0e3a7db3302429b95057a9e3d3e84ec
taknaja <peerapolkoeram@gmail.com>
2025-02-26 17:33:58 +0700

    init project

---
+++ src/main/java/takee/dev/swagger_springboot/SwaggerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package takee.dev.swagger_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/takee/dev/swagger_springboot/SwaggerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package takee.dev.swagger_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
