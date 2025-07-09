240b73ba56eb08268a1ca87453d108fa409885d7
taknaja <peerapolkoeram@gmail.com>
2025-07-09 21:38:15 +0700

    learn @RequestParam @RequestHeader and @Tag

--- src/main/java/takee/dev/swagger_springboot/ApiController.java
+++ src/main/java/takee/dev/swagger_springboot/ApiController.java
@@ -1,14 +1,12 @@
 package takee.dev.swagger_springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api")
+@Tag(name = "Api Controller", description = "Learning Swagger")
 public class ApiController {
 
     @GetMapping("/getRequest")
@@ -19,8 +17,28 @@ public class ApiController {
 
     @PostMapping("/postRequest")
     @Operation(summary = "ทดสอบ api post request", description = "Api จะตอบกลับตามที่ได้ request")
-    public PostModel postMethodName(@RequestBody PostModel request) {
+    public PostModel postMethodName(
+            @RequestBody PostModel request)
+    {
         return new PostModel(1, request.firstname(), request.lastname());
+    }
+
+    @PostMapping("/postRequestHeader")
+    @Operation(summary = "ทดสอบ api post request header", description = "Api จะตอบกลับตามที่ได้ request")
+    public PostModel postMethodHeader(
+            @RequestHeader("X-Custom-Header") String customHeader ,
+            @RequestBody PostModel request)
+    {
+        return new PostModel(1, request.firstname(), request.lastname());
+    }
+
+    @PostMapping("/postRequestParam")
+    @Operation(summary = "ทดสอบ api post request param", description = "Api request param")
+    public PostModel postRequstParam(
+            @RequestParam(name = "firstname", required = true) String firstname,
+            @RequestParam(name = "lastname", required = true) String lastname)
+    {
+        return new PostModel(1,firstname,lastname);
     }
 
 }
