889676b103988df2ab246a290f8498f788cdf497
Takee Dev <peerapolkoeram@gmail.com>
2026-06-13 09:54:44 +0700

    update controller

--- src/main/java/takee/dev/swagger_springboot/ApiController.java
+++ src/main/java/takee/dev/swagger_springboot/ApiController.java
@@ -2,7 +2,6 @@ package takee.dev.swagger_springboot;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +13,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class ApiController {
 
     @GetMapping("/getRequest")
-    @Operation(summary = "ทดสอบ api get request", description = "Api จะตอบกลับเป็นคำว่า Hello Word")
+    @Operation(summary = "ทดสอบ api get request", description = "Api จะตอบกลับเป็นคำว่า Hello World")
     public String getMethodName() {
-        return "Hello Word";
+        return "Hello World";
     }
 
     @PostMapping("/postRequest")
@@ -29,29 +28,32 @@ public class ApiController {
 
     @PostMapping("/postRequestHeader")
     @Operation(summary = "ทดสอบ api post request header", description = "Api จะตอบกลับตามที่ได้ request")
-    public PostModel postMethodHeader(
+    public ResponseEntity<PostModel> postMethodHeader(
             @RequestHeader("X-Custom-Header") String customHeader ,
             @RequestBody PostModel request)
     {
-        return new PostModel(1, request.firstname(), request.lastname());
+        return ResponseEntity
+                .ok()
+                .header("X-Custom-Header", customHeader)
+                .body(new PostModel(1, request.firstname(), request.lastname()));
     }
 
     @PostMapping("/postRequestParam")
     @Operation(summary = "ทดสอบ api post request param", description = "Api request param")
-    public PostModel postRequstParam(
+    public PostModel postRequestParam(
             @RequestParam(name = "firstname", required = true) String firstname,
             @RequestParam(name = "lastname", required = true) String lastname)
     {
         return new PostModel(1,firstname,lastname);
     }
 
-    @PostMapping(value = "PostRequestMultipartFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/PostRequestMultipartFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "ทดสอบ Api Multipart File", description = "Api Multipart File")
-    public ResponseEntity postRequestMultipartFile(
+    public ResponseEntity<Void> postRequestMultipartFile(
             @RequestPart(name = "File") MultipartFile file,
             @RequestPart(name = "Image") MultipartFile image
             ) {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
 }
